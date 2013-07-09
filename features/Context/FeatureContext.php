653d064cfd30fe7735fae026de1acc37132af68e
Silviu Voicu <scvroin@yahoo.com>
2013-07-09 11:13:59 +0300

    Revert "Revert "Fix autoload problem with FeatureContext class from features/Context folder and pass first step Given I am a guest""
    This reverts commit 67f60d91891ffbbcbae44a66bb0b706cfeab4060.

---
+++ features/Context/FeatureContext.php
@@ -0,0 +1,74 @@
+<?php
+namespace Context;
+
+use Behat\MinkExtension\Context\MinkContext;
+use Behat\Symfony2Extension\Context\KernelAwareInterface;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+//
+// Require 3rd-party libraries here:
+//
+   require_once 'PHPUnit/Autoload.php';
+   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Features context.
+ */
+class FeatureContext extends MinkContext implements KernelAwareInterface
+{
+    /**
+     * Kernel.
+     *
+     * @var KernelInterface
+     */
+    private $kernel;
+
+    /**
+     * Parameters.
+     *
+     * @var array
+     */
+    private $parameters;
+
+    /**
+     * Initializes context with parameters from behat.yml.
+     *
+     * @param array $parameters
+     */
+    public function __construct(array $parameters)
+    {
+        $this->parameters = $parameters;
+
+        //reader context
+        
+         $this->useContext('reader_registration', new ReaderRegistrationContext($parameters));  
+         
+         
+        
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+     public function setKernel(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function purgeDatabase()
+    {
+        $em = $this->kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $purger = new ORMPurger($em);
+        $purger->purge();
+    }
+   
+
+          
+   
+}
